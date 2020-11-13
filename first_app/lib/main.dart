a3f8fe8a481098eb5fad27739d28701e1b3fd31b
Patrick Sequeira <patrick.sequeira95@gmail.com>
2020-11-13 22:55:27 +0100

    Add questions and answers (via RaisedButtons) to app

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,14 +5,41 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex += 1;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1!'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2!'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3!'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
